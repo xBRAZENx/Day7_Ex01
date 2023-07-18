e5749bf584932fe77f0f0f3186d777139f4216e8
xBRAZENx <87162097+xBRAZENx@users.noreply.github.com>
2023-07-18 12:38:47 +0530

    Add files via upload

---
+++ ShapeAreaLib/ShapeAreaLib/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+    public class Area
+    {
+        public double Rectangle(double l,double b)
+        {
+            double area;
+            area = l * b;
+            return area;
+        }
+        public double Square(double s)
+        {
+            double area;
+            area = Math.Pow(s,2);
+            return area;
+        }
+        public double Circle(double r)
+        {
+            double area;
+            area = 2*3.14*Math.Pow(r,2);
+            return area;
+        }
+        public double Triangle(double b, double h)
+        {
+            double area;
+            area = 0.5*b*h;
+            return area;
+        }
+    }
+}
